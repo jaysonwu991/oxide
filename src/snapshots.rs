27694a5532d8500ac636860753a92343e66eb22e
Jayson Wu <jaysonwu991@outlook.com>
2026-09-13 09:03:05 +0800

    Set fallback git identity for snapshots

--- src/snapshots.rs
+++ src/snapshots.rs
@@ -42,6 +42,7 @@ impl Snapshots {
 
     fn git(&self, args: &[&str]) -> Result<String> {
         let output = Command::new("git")
+            .args(["-c", "user.name=oxide", "-c", "user.email=oxide@localhost"])
             .arg(format!("--git-dir={}", self.git_dir.display()))
             .arg(format!("--work-tree={}", self.work_tree.display()))
             .args(args)
